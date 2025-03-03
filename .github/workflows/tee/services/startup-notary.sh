2d2fb3baa429e4463629d9c01303d0380fa4568b
mattes <1240531+mattes@users.noreply.github.com>
2025-03-03 13:15:57 -0800

    simple http server

---
+++ .github/workflows/tee/services/startup-notary.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+set -ex
+
+cargo build --release
---
+++ .github/workflows/tee/startup-script.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+set -ex
+
+export PROJECT_NAME=$(curl -s "http://169.254.169.254/computeMetadata/v1/project/project-id" -H "Metadata-Flavor: Google")
+export GIT_HASH=$(curl -s "http://169.254.169.254/computeMetadata/v1/instance/attributes/git-hash" -H "Metadata-Flavor: Google")
+export GIT_BRANCH=$(curl -s "http://169.254.169.254/computeMetadata/v1/instance/attributes/git-branch" -H "Metadata-Flavor: Google")
+export DOMAIN=$(curl -s "http://169.254.169.254/computeMetadata/v1/instance/attributes/domain" -H "Metadata-Flavor: Google")
+
+echo "Installing Rust"
+curl https://sh.rustup.rs -sSf | sh -s -- -y
+
+mkdir /opt/tee
+
+if ! id -u tee &>/dev/null; then
+  useradd --system --shell /usr/sbin/nologin tee
+else
+  log "User 'tee' already exists"
+fi
+
+git clone --depth 1 --branch feat/github-tee-deploy-2 https://github.com/pluto/tee-web-prover.git /opt/tee/tee-web-prover
+cd /opt/tee/tee-web-prover
+
+# Run startup scripts
+for startupscript in $(find .github/workflows/tee/services -name "startup-*.sh"); do
+  echo "Processing $startupscript"
+  $startupscript
+done
+
+chown -R tee:tee /opt/tee
+
+# Enable and start services
+for servicefile in $(find .github/workflows/tee/services -name "*.service"); do
+  echo "Found service file: $servicefile"
+
+  # Link service file to systemd directory
+  ln -sf "$(pwd)/$servicefile" /etc/systemd/system/
+
+  # Enable and start the service
+  systemctl daemon-reload
+  systemctl enable "$(basename "$servicefile")"
+  systemctl start "$(basename "$servicefile")"
+done
